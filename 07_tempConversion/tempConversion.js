78807a237c51bffdcc06c1edb8070dfda79e2710
Leonich77 <leonich777@rambler.ru>
2024-08-03 13:07:15 +0500

    07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (farTemp) {
+  const celTemp = (farTemp - 32) * (5 / 9);
+  return Number.isInteger(celTemp)
+    ? celTemp
+    : Number.parseFloat(celTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celTemp) {
+  const farTemp = celTemp * (9 / 5) + 32;
+  return Number.isInteger(farTemp)
+    ? farTemp
+    : Number.parseFloat(farTemp.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
