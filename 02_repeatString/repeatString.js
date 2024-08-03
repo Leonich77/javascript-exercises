71b7369a6202ddf87a2c41a34757b8a51ca52016
Leonich77 <leonich777@rambler.ru>
2024-08-03 11:19:49 +0500

    01, 02 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (typeof string !== "string" || string === " " || typeof num !== "number") {
+    return "";
+  }
+  if (num < 0) {
+    return "ERROR";
+  } else if (num < 1) {
+    return "";
+  }
+  let result = "";
+  for (let i = 0; i < num; i++) {
+    result += string;
+  }
+  return result;
 };
 
 // Do not edit below this line
