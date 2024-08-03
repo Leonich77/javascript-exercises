8be530e79887abb891ab4e81e1578c2e29e5feb8
Leonich77 <leonich777@rambler.ru>
2024-08-03 12:06:12 +0500

    03, 04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function (inputArr, ...toRemove) {
+  return inputArr.filter((item) => !toRemove.includes(item));
 };
 
 // Do not edit below this line
